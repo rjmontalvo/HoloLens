d4632fd9603a6ab5ee150f18488383d0e717dd31
Raven Montalvo <raven.montalvo@outlook.com>
2020-04-11 21:14:08 -0700

    Commit HoloTranslator & Origami Scripts

---
+++ HoloTranslator/MicrophoneManager.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class MicrophoneManager : MonoBehaviour
+{
+    //Help to access instance of this object
+    public static MicrophoneManager instance;
+
+    //AudioSource component, provides access to mic
+    private AudioSource audioSource;
+
+    //Flag indicating mic detection
+    private bool microphoneDetected;
+
+    //Component converting speech to text
+    private DictationRecognizer dictationRecognizer;
+
+    private void Awake()
+    {
+        //Set this class to behave like a singleton
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+     //Use Unity Microphone class to detect devices and setup AudioSource
+        if(Microphone.devices.Length>0)
+        {
+            Results.instance.SetMicrophoneStatus("Initializing...");
+            audioSource = GetComponent<AudioSource>();
+            microphoneDetected = true;
+        }
+        else
+        {
+            Results.instance.SetMicrophoneStatus("No Microphone Detected");
+        }
+    }
+
+    /// <summary>
+    /// Start microphone capture. Debugging is delivered to the Results class
+    /// </summary>
+    public void StartCapturingAudio()
+    {
+        if(microphoneDetected)
+        {
+            //Start dictation
+            dictationRecognizer = new DictationRecognizer();
+            dictationRecognizer.DictationResult += DictationRecognizer_DictationResult;
+            dictationRecognizer.Start();
+
+            //Update the UI with mic status
+            Results.instance.SetMicrophoneStatus("Capturing...");
+        }
+    }
+
+    /// <summary>
+    /// Stop mic capture. Debugging message passed to Results class.
+    /// </summary>
+    public void StopCapturingAudio()
+    {
+        Results.instance.SetMicrophoneStatus("Mic Sleeping");
+        Microphone.End(null);
+        dictationRecognizer.DictationResult -= DictationRecognizer_DictationResult;
+        dictationRecognizer.Dispose();
+    }
+
+    /// <summary>
+    /// This handler is called every time the Dictation detects a pause in the speech
+    /// Debugging message passed to Results class
+    /// </summary>
+    private void DictationRecognizer_DictationResult(string text, ConfidenceLevel confidence)
+    {
+        //Update UI with dication captured
+        Results.instance.SetDictationResult(text);
+
+        //Start the coroutine that process the dictation through Azure
+        StartCoroutine(Translator.instance.TranslateWithUnityNetworking(text));
+
+    }
+}
---
+++ HoloTranslator/Translator.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Xml.Linq;
+using UnityEngine.Networking;
+
+public class Translator : MonoBehaviour
+{
+
+    public static Translator instance;
+    private string translationTokenEndpoint = "https://api.cognitive.microsoft.com/sts/v1.0/issueToken";
+    private string translationTextEndpoint = "https://api.microsofttranslator.com/v2/http.svc/Translate?";
+    private const string ocpApimSubscriptionKeyHeader = "Ocp-Apim-Subscription-Key";
+
+    private const string authorizationKey = "5ba93aa406a04466996a617371a50487";
+    private string authorizationToken;
+
+    /*languages set below are:
+     * English
+     * German
+     * Japanese
+     * Spanish
+     * French
+     */
+
+    public enum Languages { en, de, ja, es, fr}
+    public Languages from = Languages.en;
+    public Languages to = Languages.de;
+
+    private void Awake()
+    {
+        //Set this class to behave like a singleton
+        instance = this;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        //When the application starts, request an authorization token
+        StartCoroutine("GetTokenCoroutine", authorizationKey);
+    }
+    /// <summary>
+    /// Request a Token from Azure Translation Service by providing the access key
+    /// </summary>
+    private IEnumerator GetTokenCoroutine(string key)
+    {
+        if (string.IsNullOrEmpty(key))
+        {
+            throw new InvalidOperationException("Authorization key not set.");
+        }
+
+        using (UnityWebRequest unityWebRequest = UnityWebRequest.Post(translationTokenEndpoint, string.Empty))
+        {
+            unityWebRequest.SetRequestHeader("Ocp-Apim-Subscription-Key", key);
+            yield return unityWebRequest.SendWebRequest();
+
+            long responseCode = unityWebRequest.responseCode;
+
+            //Update the UI with the response code
+            Results.instance.SetAzureResponse(responseCode.ToString());
+
+            if(unityWebRequest.isNetworkError || unityWebRequest.isHttpError)
+            {
+                Results.instance.azureResponseText.text = unityWebRequest.error;
+                yield return null;
+            }
+            else
+            {
+                authorizationToken = unityWebRequest.downloadHandler.text;
+            }
+        }
+
+        //After receiving the token, begin capturing Audio with the MicrophoneManager class
+        MicrophoneManager.instance.StartCapturingAudio();
+    }
+
+    /// <summary>
+    /// Request a translation from Azure Translation Service by providing a string
+    /// Debugging result passed to Results class
+    /// </summary>
+    public IEnumerator TranslateWithUnityNetworking(string text)
+    {
+        //This query string will contain parameters for the translation
+        string queryString = 
+            string.Concat("text=", Uri.EscapeDataString(text), "&from=", from, "&to=", to);
+
+        using(UnityWebRequest unityWebRequest = UnityWebRequest.Get(translationTextEndpoint + queryString))
+        {
+            unityWebRequest.SetRequestHeader("Authorization", "Bearer " + authorizationToken);
+            unityWebRequest.SetRequestHeader("Accept", "application/xml");
+            yield return unityWebRequest.SendWebRequest();
+
+            if(unityWebRequest.isNetworkError || unityWebRequest.isHttpError)
+            {
+                yield return null;
+            }
+
+            //Parse out the response text from the returned Xml
+            string result = XElement.Parse(unityWebRequest.downloadHandler.text).Value;
+            Results.instance.SetTranslatedResult(result);
+        }
+    }
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+
+    //}
+}
